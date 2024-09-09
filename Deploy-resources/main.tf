586b6d84f405a95fe2f2238e5ed1af1bb6babf7e
Rashmina Amarasinghe <shasheenrashmina@gmail.com>
2024-09-09 23:09:42 +0530

    backend

--- Deploy-resources/main.tf
+++ Deploy-resources/main.tf
@@ -5,6 +5,11 @@ terraform {
       version = "~> 5.0"
     }
   }
+  backend "s3" {
+    bucket = "rashmina-terraform-state "
+    key = "state/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
 provider "aws" {
   region     = "us-east-1"
